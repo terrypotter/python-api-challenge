6043f9583fca36e3ea26f77f11c8ce7e8d3f36b4
Terry Potter <tepotter@yahoo.com>
2020-03-11 10:28:47 -0500

    Revision heatmap

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "38f73c0a2a9fed0579060dd556962023"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCPGH_84EzOzgxWk5qjNdV0uTwTEOXm5mI"
